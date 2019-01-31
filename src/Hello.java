4eaf3e1ef43630092ac4ae70de8ee5ec20f00b2a
anariky1 <ananthgithub@gmail.com>
2019-01-31 09:45:20 +0530

    first commit

---
+++ src/Hello.java
@@ -0,0 +1,13 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		
+		for(int i=0;i<=15;i++){
+			System.out.println("Hello world..."+i);
+			System.out.println("hi...."+i);
+    	}
+
+	}
+
+}
